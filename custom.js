5b3496ccbab577d94c2bf0356b98049085a4aa8c
Milan Stamenkovic <48022403+MilanStamen91@users.noreply.github.com>
2021-03-18 13:35:49 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,29 @@
+// This is a workaround because Codepen was having some issues loading the
+// JavaScript file from a separate pen. 🤷
+const workerAsText = document.querySelector('#worker').textContent;
+const workerAsBlob = new Blob([workerAsText], { type: 'text/javascript' });
+const ImageLoaderWorker = new Worker(URL.createObjectURL(workerAsBlob));
+
+// This is how we would *normally* load the web worker
+// const ImageLoaderWorker = new Worker('https://s.codepen.io/trezy/pen/dEBYXV.js')
+
+const imgElements = document.querySelectorAll('img[data-src]');
+
+ImageLoaderWorker.addEventListener('message', event => {
+  // Grab the message data from the event
+  const imageData = event.data;
+
+  console.log('We got a message back!', imageData);
+
+  const imageElement = document.querySelector(`img[data-src='${imageData.imageURL}']`);
+
+  const objectURL = URL.createObjectURL(imageData.blob);
+
+  imageElement.setAttribute('src', objectURL);
+  imageElement.removeAttribute('data-src');
+});
+
+imgElements.forEach(imageElement => {
+  const imageURL = imageElement.getAttribute('data-src');
+  ImageLoaderWorker.postMessage(imageURL);
+});
